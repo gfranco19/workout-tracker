d2ead0fc64b7e1248ffc9fe4994c404c2588b2fd
Giovanny Franco <gfranco19@live.com>
2020-12-06 21:06:25 -0800

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 //routes
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js")
